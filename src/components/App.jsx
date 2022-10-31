ef3f7eb512a4b526c88f210207a88ff7be4f697c
Achyut <aoogleak@gmail.com>
2022-10-31 20:42:17 +0545

    2.0 Updated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,13 +3,37 @@ import Header from "./Header";
 import Footer from "./Footer";
 
 function App() {
+  const username = "Mr. Koirala";
+
+  const date = new Date();
+  const currentTime = date.getHours();
+
+  let greetings;
+  var customStyle = {
+    color: "",
+  };
+
+  if (currentTime < 12) {
+    greetings = "Good Morning!";
+    customStyle.color = "orange";
+  } else if (currentTime < 18) {
+    greetings = "Good Afternoon!";
+    customStyle.color = "red";
+  } else {
+    greetings = "Good Night!";
+    customStyle.color = "green";
+  }
+
   return (
     <div>
       <div>
         <Header />
       </div>
       <div>
-        <h1>Welcome to the App!</h1>
+        <h1 style={{ color: "black", border: "1px solid black" }}>
+          Hello, {username}
+        </h1>
+        <h3 style={customStyle}>{greetings}</h3>
       </div>
       <div>
         <Footer />
